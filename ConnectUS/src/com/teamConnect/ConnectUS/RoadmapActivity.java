040934b7d0853b9c1b235f419a080ebaac5bd3d8
jbeaulieu <jbeaulieu@mit.edu>
2015-03-30 09:50:06 -0400

    initial project push

---
+++ ConnectUS/src/com/teamConnect/ConnectUS/RoadmapActivity.java
@@ -0,0 +1,31 @@
+package com.teamConnect.ConnectUS;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+/**
+ * Created by Jon M Beaulieu Jr on 3/27/2015.
+ */
+public class RoadmapActivity extends Activity {
+    private TextView userDisplay;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  //Hides app title bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //Hides Notification Bar
+
+        setContentView(R.layout.roadmap);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("user");
+        userDisplay = (TextView)findViewById(R.id.textView);
+        userDisplay.setText("username: " + username);
+
+    }
+}
