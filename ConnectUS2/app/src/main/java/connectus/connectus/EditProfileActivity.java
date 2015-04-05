d1fa2c2755452a45ae10f616e35587712e8b5e0f
Emily Giurleo <egiurleo@mit.edu>
2015-04-05 18:33:12 -0400

    Making changes to edit profile

--- ConnectUS2/app/src/main/java/connectus/connectus/EditProfileActivity.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/EditProfileActivity.java
@@ -1,7 +1,9 @@
 package connectus.connectus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.View;
 
 /**
  * Created by egiurleo on 4/5/15.
@@ -15,6 +17,11 @@ public class EditProfileActivity extends ConnectUSActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //Hides Notification Bar
 
         setContentView(R.layout.editprofile);
+    }
+
+    public void doneEditing(View view){
+        Intent myProfileIntent = new Intent(EditProfileActivity.this, MyProfileActivity.class);
+        EditProfileActivity.this.startActivity(myProfileIntent);
     }
 
 }
