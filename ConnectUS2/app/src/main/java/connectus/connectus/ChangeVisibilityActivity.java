1dcab68ceb3f101dd8b48bf2ab1c231673f74208
Emily Giurleo <egiurleo@mit.edu>
2015-04-05 10:03:10 -0400

    Added Change Visibility view

--- ConnectUS2/app/src/main/java/connectus/connectus/ChangeVisibilityActivity.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/ChangeVisibilityActivity.java
@@ -1,11 +1,12 @@
 package connectus.connectus;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 import java.lang.Override;
+import java.lang.reflect.Array;
 
 public class ChangeVisibilityActivity extends ConnectUSActivity {
 
@@ -16,6 +17,18 @@ public class ChangeVisibilityActivity extends ConnectUSActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //Hides Notification Bar
 
         setContentView(R.layout.changevisibility);
+    }
+
+    private void setSpinners(){
+//        int[] ids = {R.id.name_spinner, R.id.email_spinner, R.id.country_spinner, R.id.language_spinner, R.id.phone_spinner};
+//
+//        for(int id : ids){
+//            Spinner spinner = (Spinner) findViewById(id);
+//            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item);
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//            spinner.setAdapter(adapter);
+//
+//        }
     }
 
 }
