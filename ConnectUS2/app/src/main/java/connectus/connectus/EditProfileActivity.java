c4066b26cff30a2f04d58ae6f0a335b8b9929cdb
Emily Giurleo <egiurleo@mit.edu>
2015-04-05 18:49:40 -0400

    Added country spinner and fixed text boxes

--- ConnectUS2/app/src/main/java/connectus/connectus/EditProfileActivity.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/EditProfileActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.content.Context;
 
 /**
  * Created by egiurleo on 4/5/15.
@@ -17,11 +19,13 @@ public class EditProfileActivity extends ConnectUSActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //Hides Notification Bar
 
         setContentView(R.layout.editprofile);
+
+        InputMethodManager im = (InputMethodManager) this.getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        im.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
     public void doneEditing(View view){
-        Intent myProfileIntent = new Intent(EditProfileActivity.this, MyProfileActivity.class);
-        EditProfileActivity.this.startActivity(myProfileIntent);
+        this.finish();
     }
 
 }
