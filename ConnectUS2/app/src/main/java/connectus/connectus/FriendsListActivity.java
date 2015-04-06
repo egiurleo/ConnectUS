bdd4e53a0c9480bc31fd322f77e32e8b3be5aaf9
barbara42 <barbara.raven42@gmail.com>
2015-04-06 19:33:41 -0400

    added placeholder friends for friendslist

--- ConnectUS2/app/src/main/java/connectus/connectus/FriendsListActivity.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/FriendsListActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class FriendsListActivity extends ConnectUSActivity {
@@ -23,6 +25,9 @@ public class FriendsListActivity extends ConnectUSActivity {
         //get user object and display stuff based on that
         Intent intent = getIntent();
         username = intent.getStringExtra("user");
+
+        //load friends onto screen
+        showFriends();
     }
 
     public void findFriendsClick(View view){
@@ -30,4 +35,27 @@ public class FriendsListActivity extends ConnectUSActivity {
         findFriendsIntent.putExtra("user", username);     //Pass username to next activity
         FriendsListActivity.this.startActivity(findFriendsIntent);
     }
+
+    public void showFriends(){
+        String[] placeholderNames = {"Name 1", "Name 2", "Name 3"};
+
+        ScrollView sv = (ScrollView) this.findViewById(R.id.scrollView);
+
+        // Create a LinearLayout element
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        for (String name : placeholderNames){
+            // Add text
+            TextView tv = new TextView(this);
+            tv.setText(name);
+            ll.addView(tv);
+        }
+
+        // Add the LinearLayout element to the ScrollView
+        sv.addView(ll);
+        // Display the view
+        setContentView(R.layout.friendslist);
+    }
+
 }
