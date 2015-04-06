fba5cad3dd0f9d9fd6deae9c37af47e422f66b41
Shelby Pefley <spefley@mit.edu>
2015-04-05 23:28:12 -0400

    Get Login Working

--- ConnectUS2/app/src/main/java/connectus/connectus/GoogleLoginActivity.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/GoogleLoginActivity.java
@@ -12,6 +12,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -20,6 +21,7 @@ public class GoogleLoginActivity extends FragmentActivity {
     private LoginButton loginButton;
     private TextView message;
     private CallbackManager callbackManager;
+    private static final int request_code = 5;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,20 +29,24 @@ public class GoogleLoginActivity extends FragmentActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);  //Hides app title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //Hides Notification Bar
-        FacebookSdk.sdkInitialize(getApplicationContext());
 
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.login);
 
-        callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("user_friends");
+        loginButton.setReadPermissions("email");
+        LoginManager.getInstance().logOut();
+
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         // App code
                         Log.e("Login", "SUCCESS!");
-                        startNextActivity();
+                        Profile curProfile = Profile.getCurrentProfile();
+                        startNextActivity(curProfile);
                     }
 
                     @Override
@@ -57,10 +63,17 @@ public class GoogleLoginActivity extends FragmentActivity {
                 });
 
     }
-    public void startNextActivity() {
+    public void startNextActivity(Profile user) {
         Intent intent = new Intent(this, RoadmapActivity.class);
-        startActivity(intent);
+        Log.e("firstName", user.getFirstName());
+        startActivityForResult(intent, request_code);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
