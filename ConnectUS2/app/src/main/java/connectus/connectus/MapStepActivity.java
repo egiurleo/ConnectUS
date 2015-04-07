86aba49e23c3c9ce0614b1b935868637bee60202
Emily Giurleo <egiurleo@mit.edu>
2015-04-06 22:57:57 -0400

    Map now loads different detail pages.

--- ConnectUS2/app/src/main/java/connectus/connectus/MapStepActivity.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/MapStepActivity.java
@@ -2,29 +2,41 @@ package connectus.connectus;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.WindowManager;
-import android.app.Activity;
+import android.widget.TextView;
 
 /**
  * Created by egiurleo on 4/4/15.
  */
 public class MapStepActivity extends ConnectUSActivity {
 
-    private String username;
+    private String buttonId;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //Hides Notification Bar
-
         setContentView(R.layout.mapstep);
 
-        //get user object and display stuff based on that
         Intent intent = getIntent();
-        username = intent.getStringExtra("user");
+        buttonId = intent.getStringExtra("buttonID");
+
+        createView();
+    }
+
+    public void createView(){
+
+        TextView detailsTitle = (TextView) findViewById(R.id.details_title);
+        TextView detailsText = (TextView) findViewById(R.id.details_text);
+
+        System.out.println(buttonId + "_title");
+
+        int titleId = getResources().getIdentifier(buttonId + "_title", "string", getApplicationContext().getPackageName());
+        int textId = getResources().getIdentifier(buttonId + "_text", "string", getApplicationContext().getPackageName());
+
+        detailsTitle.setText(titleId);
+        detailsText.setText(textId);
 
     }
 
--- ConnectUS2/app/src/main/java/connectus/connectus/RoadmapActivity.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/RoadmapActivity.java
@@ -1,15 +1,11 @@
 package connectus.connectus;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
 import android.view.View;
 import android.widget.TextView;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.support.v4.view.ViewPager;
 
 /**
  * Created by Jon M Beaulieu Jr on 3/27/2015.
@@ -48,8 +44,7 @@ public class RoadmapActivity extends ConnectUSActivity {
 
     public void mapStepClick(View view){
         Intent mapStepIntent = new Intent(RoadmapActivity.this, MapStepActivity.class);
-        mapStepIntent.putExtra("user", username);
-
+        mapStepIntent.putExtra("buttonID", getResources().getResourceEntryName(view.getId()));
         RoadmapActivity.this.startActivity(mapStepIntent);
     }
 
