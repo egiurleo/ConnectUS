ba872075e6c5e73eebf9bd6f1e51c24424ad7915
Emily Giurleo <egiurleo@mit.edu>
2015-04-16 18:40:31 -0400

    Friendslist activity now working

--- ConnectUS2/app/src/main/java/connectus/connectus/AsyncFriendsList.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/AsyncFriendsList.java
@@ -22,23 +22,21 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by egiurleo on 4/16/15.
  */
-public class AsyncFindFriends extends AsyncTask<Void, Void, Void> {
+public class AsyncFriendsList extends AsyncTask<Void, Void, Void> {
 
     private Context context;
     private final Activity activity;
     private String[] friendsList;
     private String userId;
     private OnClickListener onClickFriend;
-    private String[] idList;
     private String[] names;
 
-    public AsyncFindFriends(Context context, Activity activity){
+    public AsyncFriendsList(Context context, Activity activity){
         this.context = context;
         this.activity = activity;
 
@@ -46,10 +44,10 @@ public class AsyncFindFriends extends AsyncTask<Void, Void, Void> {
             @Override
             public void onClick(View v) {
                 String tag = (String) v.getTag();
-                Intent otherProfileIntent = new Intent(AsyncFindFriends.this.activity, OtherProfileActivity.class);
+                Intent otherProfileIntent = new Intent(AsyncFriendsList.this.activity, OtherProfileActivity.class);
                 otherProfileIntent.putExtra("id", tag);
 
-                AsyncFindFriends.this.activity.startActivity(otherProfileIntent);
+                AsyncFriendsList.this.activity.startActivity(otherProfileIntent);
 
             }
         };
@@ -74,6 +72,8 @@ public class AsyncFindFriends extends AsyncTask<Void, Void, Void> {
             friendsList = userInfo[8].split(" ");
             userId = userInfo[0];
 
+            Log.e("friends list", friendsList[0]);
+
 
         } catch (IOException e){
             Log.e("Error: ", "file not found");
@@ -81,26 +81,11 @@ public class AsyncFindFriends extends AsyncTask<Void, Void, Void> {
 
         HttpClient httpclient = new DefaultHttpClient();
 
-        try {
-            HttpGet httprequest = new HttpGet("http://egiurleo.scripts.mit.edu/getAllIds.php");
-            HttpResponse response = httpclient.execute(httprequest);
-
-            if(response != null){
-                InputStream inputStream2 = response.getEntity().getContent();
-
-                //return the string to be cached
-                String x = convertStreamToString(inputStream2);
-                idList = x.split(" ");
-                idList = Arrays.copyOfRange(idList, 1, idList.length);
-            }
-        }catch (IOException e){
-            Log.e("Exception", "IOException");
-        }
-
-        names = new String[idList.length];
-        for(int i=0; i<idList.length; i++){
+        names = new String[friendsList.length];
+        for(int i=0; i<friendsList.length; i++){
+            Log.e("friend", friendsList[i]);
             try {
-                HttpGet httprequest = new HttpGet("http://egiurleo.scripts.mit.edu/getName.php?userId=" + idList[i]);
+                HttpGet httprequest = new HttpGet("http://egiurleo.scripts.mit.edu/getName.php?userId=" + friendsList[i]);
                 HttpResponse response = httpclient.execute(httprequest);
 
                 if(response != null){
@@ -122,31 +107,29 @@ public class AsyncFindFriends extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected void onPostExecute(Void result){
-        LinearLayout findFriendsContainer = (LinearLayout) activity.findViewById(R.id.find_friends_container);
+        LinearLayout friendsContainer = (LinearLayout) activity.findViewById(R.id.friends);
 
-        for(int i=0; i<idList.length; i++){
-            if(!idList[i].equals(userId) && !Arrays.asList(friendsList).contains(idList[i])){
+        for(int i=0; i<friendsList.length; i++){
+            //create linear layout
+            LinearLayout layout = new LinearLayout(activity);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+            params.setMargins(10, 10, 10, 0);
+            layout.setPadding(10, 10, 10, 10);
+            layout.setBackgroundColor(Color.parseColor("#DDDDDD"));
+            layout.setOnClickListener(onClickFriend);
+            layout.setTag(friendsList[i]);
 
-                //create linear layout
-                LinearLayout layout = new LinearLayout(activity);
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                        new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-                params.setMargins(10, 10, 10, 0);
-                layout.setPadding(10, 10, 10, 10);
-                layout.setBackgroundColor(Color.parseColor("#DDDDDD"));
-                layout.setOnClickListener(onClickFriend);
-                layout.setTag(idList[i]);
+            //create textview
+            TextView textView = new TextView(activity);
+            textView.setText(names[i]);
+            LayoutParams textViewLayoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+            textView.setLayoutParams(textViewLayoutParams);
+            textView.setTextSize(20);
 
-                //create textview
-                TextView textView = new TextView(activity);
-                textView.setText(names[i]);
-                LayoutParams textViewLayoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-                textView.setLayoutParams(textViewLayoutParams);
-                textView.setTextSize(20);
+            layout.addView(textView);
+            friendsContainer.addView(layout, params);
 
-                layout.addView(textView);
-                findFriendsContainer.addView(layout, params);
-            }
         }
 
     }
--- ConnectUS2/app/src/main/java/connectus/connectus/FriendsListActivity.java
+++ ConnectUS2/app/src/main/java/connectus/connectus/FriendsListActivity.java
@@ -1,18 +1,11 @@
 package connectus.connectus;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
+import android.view.WindowManager;
 
 public class FriendsListActivity extends ConnectUSActivity {
-
-    private String username;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,45 +14,14 @@ public class FriendsListActivity extends ConnectUSActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //Hides Notification Bar
         setContentView(R.layout.friendslist);
 
-        //get user object and display stuff based on that
-        Intent intent = getIntent();
-        username = intent.getStringExtra("user");
-
         //load friends onto screen
-        showFriends();
+        AsyncFriendsList asyncFriendsList = new AsyncFriendsList(getApplicationContext(), FriendsListActivity.this);
+        asyncFriendsList.execute();
     }
 
     public void findFriendsClick(View view){
         Intent findFriendsIntent = new Intent(FriendsListActivity.this, FindFriendsActivity.class);
-        findFriendsIntent.putExtra("user", username);     //Pass username to next activity
         FriendsListActivity.this.startActivity(findFriendsIntent);
-    }
-
-    public void showFriends(){
-
-        //taking this out for now
-        //will put it back when we include server functionality
-
-//        String[] placeholderNames = {"Name 1", "Name 2", "Name 3"};
-//
-//        LinearLayout ll = (LinearLayout) this.findViewById(R.id.friends);
-//
-//        for (String name : placeholderNames){
-//            // Add text
-//            TextView tv = new TextView(this);
-//            tv.setText(name);
-//            ll.addView(tv);
-//        }
-
-        // Display the view
-        //setContentView(R.layout.friendslist);
-    }
-
-    public void friendsProfile(View view){
-        Intent otherProfileIntent = new Intent(this, OtherProfileActivity.class);
-        otherProfileIntent.putExtra("user", username);
-
-        this.startActivity(otherProfileIntent);
     }
 
 }
