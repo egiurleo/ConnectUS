7ee12b76ad3e34183f4c1869b2af816977a48d60
jbeaulieu <jbeaulieu@mit.edu>
2015-04-18 19:03:34 -0400

    testing log features

---
+++ ConnectUS2/app/src/main/java/connectus/connectus/LogTest.java
@@ -0,0 +1,66 @@
+package com.rdc;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.TextView;
+
+import connectus.connectus.ConnectUSActivity;
+
+public class LogTest extends ConnectUSActivity {
+
+    private StringBuilder log;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        try {
+            Process process = Runtime.getRuntime().exec("logcat -d");
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            log=new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                log.append(line);
+            }
+            TextView tv = (TextView)findViewById(R.id.textView1);
+            tv.setText(log.toString());
+        } catch (IOException e) {
+        }
+
+
+        //convert log to string
+        final String logString = new String(log.toString());
+
+        //create text file in SDCard
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File (sdCard.getAbsolutePath() + "/myLogcat");
+        dir.mkdirs();
+        File file = new File(dir, "logcat.txt");
+
+        try {
+            //to write logcat in text file
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+
+            // Write the string to the file
+            osw.write(logString);
+            osw.flush();
+            osw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
